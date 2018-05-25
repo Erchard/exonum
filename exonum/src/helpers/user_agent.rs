d7353b107ee6a0b5fbad9276cf2a4dfd073947c1
arsenguzhva@gmail.com <arsenguzhva@gmail.com>
2018-05-25 15:12:59 +0300

    FreeBSD

--- exonum/src/helpers/user_agent.rs
+++ exonum/src/helpers/user_agent.rs
@@ -14,7 +14,6 @@
 
 //! Information about current node including Exonum, Rust and OS versions.
 
-use os_info;
 
 static USER_AGENT: &str = include_str!(concat!(env!("OUT_DIR"), "/user_agent"));
 
@@ -29,23 +28,13 @@ static USER_AGENT: &str = include_str!(concat!(env!("OUT_DIR"), "/user_agent"));
 /// println!("{}", user_agent);
 /// ```
 pub fn get() -> String {
-    let os = os_info::get();
-    format!("{}/{}", USER_AGENT, os)
+
+    format!("{}/{}", USER_AGENT, 0)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // Checks that user agent string contains three nonempty components.
-    #[test]
-    fn components() {
-        let user_agent = get();
-        let components: Vec<_> = user_agent.split('/').collect();
-        assert_eq!(components.len(), 3);
 
-        for val in components {
-            assert!(!val.is_empty());
-        }
-    }
 }
